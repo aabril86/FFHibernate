68f46d0a265b563638ba0f1385056ce405f421e2
aitorsp-elpuig <aitorsp@elpuig.xeill.net>
2021-04-08 17:29:48 +0200

    Mazo

--- src/main/java/FF/Entities/Mazo.java
+++ src/main/java/FF/Entities/Mazo.java
@@ -1,13 +1,50 @@
 package FF.Entities;
 
-import FF.Entities.Cartas.Carta;
+import Entities.Carta;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
-public class Mazo {
+public class Mazo implements Serializable {
+    private int id_mazo;
 
-    int id_mazo;
+    private List<Entities.Carta> cartas = new ArrayList<Entities.Carta> ();
 
-    Personaje personaje;
-    ArrayList<Carta> cartas = new ArrayList<>();
+
+    public Mazo(int id_mazo) {
+        super();
+        this.id_mazo = id_mazo;
+    }
+    public Mazo() {
+        super();
+    }
+    public int getId_mazo() {
+        return id_mazo;
+    }
+    public void setId_mazo(int id_mazo) {
+        this.id_mazo = id_mazo;
+    }
+    public void addCarta(Entities.Carta art){
+        cartas.add(art);
+    }
+
+    public Entities.Carta getCarta(int i){
+        return cartas.get(i);
+    }
+
+    public List<Entities.Carta> getCartas() {
+        return cartas;
+    }
+    public void setCartas(List<Carta> cartas) {
+        this.cartas = cartas;
+    }
+
+    @Override
+    public String toString() {
+        return "Mazo{" +
+                "id_mazo=" + id_mazo +
+                ", cartas=" + cartas +
+                '}';
+    }
 }
