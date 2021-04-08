9eed1964e2438fa0c4eaccc50eb060b8ef0a8031
nanonixx <nhid@elpuig.xeill.net>
2021-04-08 17:13:13 +0200

    arhoa cona nrar<y lsit

--- src/main/java/FF/Entities/Mazo.java
+++ src/main/java/FF/Entities/Mazo.java
@@ -1,4 +1,13 @@
 package FF.Entities;
 
+import FF.Entities.Cartas.Carta;
+
+import java.util.ArrayList;
+
 public class Mazo {
+
+    int id_mazo;
+
+    Personaje personaje;
+    ArrayList<Carta> cartas = new ArrayList<>();
 }
--- src/main/java/FF/Entities/Personaje.java
+++ src/main/java/FF/Entities/Personaje.java
@@ -1,4 +1,12 @@
 package FF.Entities;
 
+import FF.Entities.Cartas.Carta;
+
+import java.util.ArrayList;
+
 public class Personaje {
+    int id_personaje, vida, mana;
+    String nombre, sprite, categoria;
+
+    ArrayList<Carta> mano = new ArrayList<>();
 }
